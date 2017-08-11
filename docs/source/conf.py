2f0b2ffd8cff72522593260c07cb35c2b0a5224d
maartenbreddels <maartenbreddels@gmail.com>
2017-08-11 12:12:22 +0200

    docs: added ipyvolume.mp4 as demo file

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -42,6 +42,7 @@ extensions = ['sphinx.ext.autodoc',
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
+html_extra_path = ['ipyvolume.mp4']
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 #

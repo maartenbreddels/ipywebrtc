d87d4ac4ede9bdf0874c125a3282b7c15240c1d1
maartenbreddels <maartenbreddels@gmail.com>
2017-08-11 11:40:26 +0200

    fix: support for firefox/mozCaptureStream

--- js/src/webrtc.js
+++ js/src/webrtc.js
@@ -107,20 +107,26 @@ var VideoStreamModel = MediaStreamModel.extend({
         this.stream = new Promise((resolve, reject) => {
             that.stream_resolved = resolve;
         })
-        if(!this.video.captureStream) {
-            console.log('captureStream not supported for this browser')
-        } else {
+        if(this.video.captureStream || this.video.mozCaptureStream) {
             // following https://github.com/webrtc/samples/blob/gh-pages/src/content/capture/video-pc/js/main.js
             var make_stream = _.once(_.bind(function() {
-                this.stream_resolved(this.video.captureStream())
+                if(this.video.captureStream) {
+                    this.stream_resolved(this.video.captureStream())
+                } else if(this.video.mozCaptureStream) {
+                    console.info('falling back to mozCaptureStream')
+                    this.stream_resolved(this.video.mozCaptureStream())
+                }
             }, this))
-            this.video.oncanplay = make_stream
+            //this.video.oncanplay = make_stream
+            this.video.onplay = make_stream
             if(this.video.readyState >= 3) {
                 make_stream()
             }
             //this.stream = Promise.resolve(this.video.captureStream())
             this.update_play()
             this.update_loop()
+        } else {
+            console.log('captureStream not supported for this browser')
         }
         this.on('change:play', this.update_play, this)
         this.on('change:loop', this.update_loop, this)

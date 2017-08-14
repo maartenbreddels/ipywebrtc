11cbfa67d398122e1186aa681f892a28b2449582
maartenbreddels <maartenbreddels@gmail.com>
2017-08-14 11:54:32 +0200

    new: easy ipywebrtc.chat function to set up a room instantly

--- ipywebrtc/__init__.py
+++ ipywebrtc/__init__.py
@@ -1,5 +1,4 @@
 from ._version import version_info, __version__
-
 from .webrtc import *
 
 def _jupyter_nbextension_paths():
@@ -9,3 +8,20 @@ def _jupyter_nbextension_paths():
         'dest': 'jupyter-webrtc',
         'require': 'jupyter-webrtc/extension'
     }]
+
+def _random_room():
+    import random
+    return "".join(chr(ord('0')+random.randint(0, 9)) for k in range(6))
+
+def chat(room=None, stream=None, **kwargs):
+    from IPython.display import display
+    if room is None:
+        room = _random_room()
+        print("room =", room)
+    if stream is None:
+        stream = CameraStream()
+    room = ipywebrtc.WebRTCRoomMqtt(stream=stream, room=room)
+    box = widgets.HBox(children=[])
+    widgets.jslink((room, 'streams'), (box, 'children'))
+    display(box)
+    return room

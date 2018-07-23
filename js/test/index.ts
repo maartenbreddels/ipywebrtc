c87bff8623da0f838854930d3798a9905dd01d5e
Maarten Breddels <maartenbreddels@gmail.com>
2018-07-23 10:39:07 +0200

    set up testing and use typescript to compile to es5

---
+++ js/test/index.ts
@@ -0,0 +1,2 @@
+// import all tests here, otherwise if we include them in karma.conf.js it will all be separate bundles
+import './mediastream.ts';
--- js/webpack.config.js
+++ js/webpack.config.js
@@ -1,71 +1,64 @@
 var version = require('./package.json').version;
 
-// Custom webpack loaders are generally the same for all webpack bundles, hence
-// stored in a separate local variable.
-var loaders = [
-    { test: /\.json$/, loader: 'json-loader' },
-];
+// // Custom webpack loaders are generally the same for all webpack bundles, hence
+// // stored in a separate local variable.
+// var loaders = [
+//     { test: /\.json$/, loader: 'json-loader' },
+// ];
 
+const path = require('path');
+
+var rules = [
+    // { test: /\.json$/, use: "json-loader" },
+    {test: /\.js$/,  use: [{loader: "ts-loader", options: {transpileOnly: true} }]}
+];
+var externals = ['@jupyter-widgets/base', '@jupyter-widgets/controls', 'jupyter-js-widgets']
+var pyname = 'ipywebrtc'
+
+var resolve =  {
+    extensions: ['.ts', '.js']
+};
 
 module.exports = [
-    {// Notebook extension
-     //
-     // This bundle only contains the part of the JavaScript that is run on
-     // load of the notebook. This section generally only performs
-     // some configuration for requirejs, and provides the legacy
-     // "load_ipython_extension" function which is required for any notebook
-     // extension.
-     //
+    {
         entry: './src/extension.js',
         output: {
             filename: 'extension.js',
-            path: '../ipywebrtc/static',
+            path: path.resolve(__dirname, `../${pyname}/static`),
             libraryTarget: 'amd'
-        }
+        },
+        mode: 'development',
+        resolve: resolve
     },
-    {// Bundle for the notebook containing the custom widget views and models
-     //
-     // This bundle contains the implementation for the custom widget views and
-     // custom widget.
-     // It must be an amd module
-     //
+    {
         entry: './src/index.js',
         output: {
             filename: 'index.js',
-            path: '../ipywebrtc/static',
+            path: path.resolve(__dirname, `../${pyname}/static`),
             libraryTarget: 'amd'
         },
         devtool: 'source-map',
         module: {
-            loaders: loaders
+            rules: rules
         },
-        externals: ['@jupyter-widgets/base', '@jupyter-widgets/controls', 'jupyter-js-widgets']
+        externals: externals,
+        mode: 'development',
+        resolve: resolve
     },
     {// Embeddable jupyter-webrtc bundle
-     //
-     // This bundle is generally almost identical to the notebook bundle
-     // containing the custom widget views and models.
-     //
-     // The only difference is in the configuration of the webpack public path
-     // for the static assets.
-     //
-     // It will be automatically distributed by unpkg to work with the static
-     // widget embedder.
-     //
-     // The target bundle is always `dist/index.js`, which is the path required
-     // by the custom widget embedder.
-     //
         entry: './src/embed.js',
         output: {
             filename: 'index.js',
-            path: './dist/',
+            path: path.resolve(__dirname, './dist/'),
             libraryTarget: 'amd',
             publicPath: 'https://unpkg.com/jupyter-webrtc@' + version + '/dist/'
         },
         devtool: 'source-map',
         module: {
-            loaders: loaders
+            rules: rules
         },
-        externals: ['@jupyter-widgets/base', '@jupyter-widgets/controls', 'jupyter-js-widgets']
+        externals: externals,
+        mode: 'development',
+        resolve: resolve
     }
 ];

02b0cc3833d9a7a8136f13995920085735e5778a
Maarten Breddels <maartenbreddels@gmail.com>
2018-08-10 20:10:53 +0200

    require ipywidget 7.4+

--- setup.py
+++ setup.py
@@ -139,7 +139,7 @@ setup_args = {
         ]),
     ],
     'install_requires': [
-        'ipywidgets>=6.0.0',
+        'ipywidgets>=7.4.0',
     ],
     'packages': find_packages(),
     'zip_safe': False,

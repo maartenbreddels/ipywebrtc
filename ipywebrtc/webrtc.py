dff2f45dd56fc448d740d014f1c5c8e5aecf86cc
Vidar Tonaas Fauske <vidartf@gmail.com>
2018-11-15 18:08:24 +0100

    Gvie some examples of video/audio codecs

--- ipywebrtc/webrtc.py
+++ ipywebrtc/webrtc.py
@@ -402,7 +402,7 @@ class VideoRecorder(Recorder):
     _view_name = Unicode('VideoRecorderView').tag(sync=True)
 
     video = Instance(Video).tag(sync=True, **widget_serialization)
-    codecs = Unicode('', help='Optional codecs for the recording.').tag(sync=True)
+    codecs = Unicode('', help='Optional codecs for the recording, e.g. "vp8" or "vp9, opus".').tag(sync=True)
 
     @traitlets.default('video')
     def _default_video(self):
@@ -450,7 +450,7 @@ class AudioRecorder(Recorder):
     _view_name = Unicode('AudioRecorderView').tag(sync=True)
 
     audio = Instance(Audio).tag(sync=True, **widget_serialization)
-    codecs = Unicode('', help='Optional codecs for the recording.').tag(sync=True)
+    codecs = Unicode('', help='Optional codecs for the recording, e.g. "opus".').tag(sync=True)
 
     @traitlets.default('audio')
     def _default_audio(self):
